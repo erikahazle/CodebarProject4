42582b6f4c9445b0892e07fecaaa61f21f93cdae
Erika <erikajon@hotmail.co.uk>
2015-03-11 20:37:01 +0000

    added github api user info request

--- Codebar JavaScript Lesson 4/script.js
+++ Codebar JavaScript Lesson 4/script.js
@@ -1,1 +1,23 @@
 // github user finder example
+
+// assign getGithubUserInfo(username) to a variable response
+function getGithubInfo(name) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://api.github.com/users/' + name, false);
+	xhr.send();
+	return xhr;
+}
+
+
+$(document).ready(function () {
+	$(document).on('keypress', '#username', function(e) {
+		if (e.which === 13) {
+			// get val() from input field
+			var name = $('#username').val();
+
+			var response = getGithubInfo(name);
+
+			console.log(response.responseText);
+		}
+	})
+});
